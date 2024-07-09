d916a357911526d45577c1f1b49de22a650889ff
Jonnester <84779222+jon123boss@users.noreply.github.com>
2024-07-10 07:43:45 +0800

    Trying to use characterai API to access chatbots to make the basis for the project

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from characterai import aiocai, sendCode, authUser
+import asyncio
+
+async def main():
+    email = input('YOUR EMAIL: ')
+
+    code = sendCode(email)
+
+    link = input('CODE IN MAIL: ')
+
+    token = authUser(link, email)
+
+    print(f'YOUR TOKEN: {token}')
+
+asyncio.run(main())
+
 from characterai import aiocai
 import asyncio
 async def main():
