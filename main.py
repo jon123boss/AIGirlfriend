56584b20da10a90797df814101f51de83de0bf21
Jonnester <84779222+jon123boss@users.noreply.github.com>
2024-07-10 10:12:46 +0800

    Got the token and had an successful test run

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-from characterai import aiocai, sendCode, authUser
-import asyncio
-
-async def main():
-    email = input('YOUR EMAIL: ')
-
-    code = sendCode(email)
-
-    link = input('CODE IN MAIL: ')
-
-    token = authUser(link, email)
-
-    print(f'YOUR TOKEN: {token}')
-
-asyncio.run(main())
-
 from characterai import aiocai
 import asyncio
-async def main():
-    char = ""
+import pygame
 
-    client = aiocai.Client('')
+from gtts import gTTS
+tts = gTTS('hello', lang='en')
+tts.save('hello.mp3')
+# Initialize pygame mixer
+pygame.mixer.init()
+
+# Load the audio file
+pygame.mixer.music.load('hello.mp3')
+
+# Play the audio file
+pygame.mixer.music.play()
+
+async def main():
+    char = "EEI6sjnddRIJTVC59MODiYjL0-JyDIVI2IEGLkPx2Jk"
+
+    client = aiocai.Client('c3090b0e969ff9aa484638c40ca6459145223959')
 
     me = await client.get_me()
 
